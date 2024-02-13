ff32b4796c266b3b852c4f1347c6b964241c130c
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-02-13 17:58:41 -0300

    aaaa

--- Finanças.py
+++ Finanças.py
@@ -43,7 +43,11 @@ credito = load_data2(st.secrets["url_extrato_credito"])
 
 st.title('Débito')
 
-data = st.text_input('Insirir Data')
+debito_data = st.text_input('Insirir Data')
+debito_descrição =  st.text_input('Insirir Data')
+debito_classificacao = st.selectbox('Selecione o tipo:', ['Necessidade', 'Lazer - Corinthians', 'Lazer - Outros', 'Lazer - Comida', 'Comida','Casa', 'Passagem','Cabelo','Outros','Classificação'], key='class-debito')
+debito_valor = float(st.text_input('Insirir Data'))
+
 
 
 debito
