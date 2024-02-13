8dba238c62e562f3819f2af5d551a41b6358fe38
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-02-13 17:32:36 -0300

    aaaa

--- Finanças.py
+++ Finanças.py
@@ -12,3 +12,33 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from scipy.stats import poisson
+
+
+st.set_page_config(
+    page_title="Apostas",
+    layout="wide"
+)
+
+
+st.title('Página de Análise')
+
+
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("chave_api.json", scope)
+client = gspread.authorize(creds)
+
+
+
+@st.cache_data(ttl=20)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+extrato_credito = load_data(st.secrets["url_tendencias_2linhas"])
+
+@st.cache_data(ttl=20)
+def load_data2(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+extrato_debito = 
