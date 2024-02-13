ad7fe09596b2fac8866dc54934ce1d99e71ff42c
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-02-13 17:21:11 -0300

    aaaaa

---
+++ Finanças.py
@@ -0,0 +1,14 @@
+
+from calendar import c
+import streamlit as st
+import pandas as pd
+import os
+from openpyxl import load_workbook
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime, date
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from scipy.stats import poisson
