48784fed855c67e6e997d89568ede0327fe944d6
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-02-14 15:21:54 -0300

    aaaaa

--- Finanças.py
+++ Finanças.py
@@ -103,43 +103,43 @@ with tab1:
 
             #a partir do calculo de data conseguimos ter o mes e jogamos lá
 
-            debito_data = st.text_input('Insirir Data')
-            debito_descrição =  st.text_input('Insirir Descrição')
+            vr_data = st.text_input('Insirir Data')
+            vr_descrição =  st.text_input('Insirir Descrição')
+            vr_local =  st.text_input('Insirir Local')
+            vr_classificacao = st.selectbox('Selecione o tipo:', ['Almoço no escritório','Saídas','Saídas - Pitica','Rua','Casa','Outros'], key='class-vr')
+            vr_valor = st.text_input('Insirir Valor')
 
-            debito_classificacao = st.selectbox('Selecione o tipo:', ['Necessidade', 'Lazer - Corinthians', 'Lazer - Outros', 'Lazer - Comida', 'Comida','Casa', 'Passagem','Cabelo','Outros','Classificação'], key='class-debito')
-            debito_valor = st.text_input('Insirir Valor')
-
-            if debito_valor == "":
-                debito_valor = 1.0
+            if vr_valor == "":
+                vr_valor = 1.0
             else:
-                debito_valor = debito_valor
+                vr_valor = vr_valor
 
-            debito_valor = float(debito_valor)
+            vr_valor = float(vr_valor)
 
-            if debito_data  == "":
-                debito_data = "08/02/2000"
+            if vr_data  == "":
+                vr_data = "08/02/2000"
             else:
-                debito_data = debito_data    
+                vr_data = vr_data    
 
-            debito_date_obj = datetime.strptime(debito_data, "%d/%m/%Y")
-            debito_mes   = debito_date_obj.month
+            vr_date_obj = datetime.strptime(vr_data, "%d/%m/%Y")
+            vr_mes   = vr_date_obj.month
 
-            novos_debitos = []
+            novos_vrs = []
 
             with st.form('form'):
-                if st.form_submit_button('Adicionar Débito'):
-                    novo_debito = [debito_data, debito_mes, debito_descrição, debito_classificacao, debito_valor]
-                    novos_debitos.append(novo_debito)
+                if st.form_submit_button('Adicionar Gastor VR'):
+                    novo_vr = [vr_data, vr_mes, vr_descrição,vr_local,  vr_classificacao, vr_valor]
+                    novos_vrs.append(novo_vr)
 
-            if novos_debitos:
-                novos_debitos_df = pd.DataFrame(novos_debitos, columns=debito.columns)
-                worksheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1lEIcy7vjH-X7U1t-toxC6V-ps39f6WQw2mruzTfx1VE/edit#gid=0').get_worksheet(0)
+            if novos_vrs:
+                novos_vrs_df = pd.DataFrame(novos_vrs, columns=vr.columns)
+                worksheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1ZCMCzyrMdzIDkGnNZAErAyowwi8FzkivIFWhwEDyII8/edit#gid=0').get_worksheet(0)
                 
                 # Obter o número de linhas existentes na planilha
                 num_rows = len(worksheet.get_all_values())
                 
                 # Inserir os dados nas linhas subsequentes
-                values_to_insert = novos_debitos_df.values.tolist()
+                values_to_insert = novos_vrs_df.values.tolist()
                 worksheet.insert_rows(values_to_insert, num_rows + 1) 
                 vr
 
