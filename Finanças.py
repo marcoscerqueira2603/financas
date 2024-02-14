e4841b973d164f33c51eb2a650da61790f5c4db7
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-02-14 15:14:10 -0300

    aaa

--- Finanças.py
+++ Finanças.py
@@ -40,6 +40,13 @@ def load_data2(sheets_url):
 
 credito = load_data2(st.secrets["url_extrato_credito"])
 
+@st.cache_data(ttl=20)
+def load_data3(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+vr = load_data3(st.secrets["url_extrato_vr"])
+
 tab1,tab2 = st.tabs(['Incluir Dados', 'Construir'])
 
 with tab1:   
@@ -88,4 +95,51 @@ with tab1:
             # Inserir os dados nas linhas subsequentes
             values_to_insert = novos_debitos_df.values.tolist()
             worksheet.insert_rows(values_to_insert, num_rows + 1) 
+
+    with st.expander('VR'):
+            st.title('VR')
+
+            #adicionando dados relativos a aba de débito: incluem a data, a classificação, o valor, a descrição
+
+            #a partir do calculo de data conseguimos ter o mes e jogamos lá
+
+            debito_data = st.text_input('Insirir Data')
+            debito_descrição =  st.text_input('Insirir Descrição')
+
+            debito_classificacao = st.selectbox('Selecione o tipo:', ['Necessidade', 'Lazer - Corinthians', 'Lazer - Outros', 'Lazer - Comida', 'Comida','Casa', 'Passagem','Cabelo','Outros','Classificação'], key='class-debito')
+            debito_valor = st.text_input('Insirir Valor')
+
+            if debito_valor == "":
+                debito_valor = 1.0
+            else:
+                debito_valor = debito_valor
+
+            debito_valor = float(debito_valor)
+
+            if debito_data  == "":
+                debito_data = "08/02/2000"
+            else:
+                debito_data = debito_data    
+
+            debito_date_obj = datetime.strptime(debito_data, "%d/%m/%Y")
+            debito_mes   = debito_date_obj.month
+
+            novos_debitos = []
+
+            with st.form('form'):
+                if st.form_submit_button('Adicionar Débito'):
+                    novo_debito = [debito_data, debito_mes, debito_descrição, debito_classificacao, debito_valor]
+                    novos_debitos.append(novo_debito)
+
+            if novos_debitos:
+                novos_debitos_df = pd.DataFrame(novos_debitos, columns=debito.columns)
+                worksheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1lEIcy7vjH-X7U1t-toxC6V-ps39f6WQw2mruzTfx1VE/edit#gid=0').get_worksheet(0)
+                
+                # Obter o número de linhas existentes na planilha
+                num_rows = len(worksheet.get_all_values())
+                
+                # Inserir os dados nas linhas subsequentes
+                values_to_insert = novos_debitos_df.values.tolist()
+                worksheet.insert_rows(values_to_insert, num_rows + 1) 
+                vr
 
