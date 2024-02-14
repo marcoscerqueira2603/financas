701fc1777d82853b53ce4d93b2dc8bd8d12b64da
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-02-14 16:10:24 -0300

    aaa

--- Finanças.py
+++ Finanças.py
@@ -148,10 +148,11 @@ with tab1:
 
         credito_parcelas =  st.number_input('Inserir Parcelas', value=1)
         credito_mes_parcela1 =  st.number_input('Inserir Mês 1ª Parcela', value=1)
+        credito_valor = st.text_input('Insirir Valor Crédito', key = 'insirir-valor-credito')
         credito_descrição =  st.text_input('Insirir Descrição', key = 'insirir-descricao-credito')
         credito_classificacao = st.selectbox('Selecione o tipo:', ['Faturas 2023','Presente Pitica','Presentes - Família','Lazer','Roupas','Compras Minhas','Outros'], key='class-credito')
         credito_cartao = st.selectbox('Selecione o cartão:', ['Inter','Nubank','C6'], key='cartao-credito')
-        credito_valor = st.text_input('Insirir Valor Crédito', key = 'insirir-valor-credito')
+
 
         if credito_valor == "":
             credito_valor = 100.0
