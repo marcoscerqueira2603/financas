2a69b1c0d3ab8ba26903afa88a36458c3531e52e
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-03-03 23:35:25 -0300

    aaa

--- Finanças.py
+++ Finanças.py
@@ -295,8 +295,8 @@ with tab1:
         orcamento_debito = st.text_input('Insirir Orçamento débito', key = "inserir-debito-orçamento")
         orcamento_juntar = st.text_input('Insirir Orçamento juntar', key = "inserir-juntar-orçamento")
 
-        soma_gastos = orcamento_casa + orcamento_fieltorcedor + orcamento_cabelo +  orcamento_celular +  orcamento_spotify + orcamento_passagem + orcamento_segurocel+ orcamento_casa+ orcamento_tembici +orcamento_crednubank + orcamento_credinter + orcamento_debito +orcamento_juntar
-        sobra = orcamento_salario_base - soma_gastos
+        soma_gastos = float(orcamento_casa) + float(orcamento_fieltorcedor)  + float(orcamento_cabelo) +  float(orcamento_celular) +  float(orcamento_spotify) + float(orcamento_passagem) + float(orcamento_segurocel)+ float(orcamento_casa)+ float(orcamento_tembici) + float(orcamento_crednubank) + float(orcamento_credinter) + float(orcamento_debito) +float(orcamento_juntar)
+        sobra = float(orcamento_salario_base) - soma_gastos
         sobra
      
 
