34b3ed544fb40e969e700fcddf5e514dda20c068
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-03-24 13:13:08 -0300

    ss

--- Finanças.py
+++ Finanças.py
@@ -405,7 +405,7 @@ with tab3:
         grafico_debito_class_med
  
     ultima_linha_debito_mes = debito_mes_consolidado.tail(1)
-    #saldo_atual_debito_mes = ultima_linha_debito_mes['Valor Orçamento'] - ultima_linha_debito_mes['Valor']           
-    #saldo_atual_debito_mes
-    ultima_linha_debito_mes     
+    saldo_atual_debito_mes = ultima_linha_debito_mes['Valor_Orçamento'] - ultima_linha_debito_mes['Valor']           
+    saldo_atual_debito_mes
+         
     
