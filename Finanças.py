7f327787f54edc75f59dc704bfb89f4c5d9d24dc
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-03-24 11:59:51 -0300

    aaa

--- Finanças.py
+++ Finanças.py
@@ -377,7 +377,7 @@ with tab3:
     )
 
     debito_classificacao_soma = debito.groupby(['Classificação'])['Valor'].sum()
-    debito_classificacao_soma_total = debito_classificacao_soma['Valor'].sum()
+    debito_classificacao_soma_total = debito['Valor'].sum()
     debito_classificacao_soma["Percentual"] = debito_classificacao_soma['Valor']/debito_classificacao_soma_total
     debito_classificacao_soma
 
