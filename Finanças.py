ed4c213bf38fabe34d6ba09b34969e1e4da8a125
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-03-24 11:09:01 -0300

    aaa

--- Finanças.py
+++ Finanças.py
@@ -353,7 +353,7 @@ with tab3:
     orcamento_mensal_debito =  orcamento_mensal_debito.rename(columns={'Mês': 'Mês Referência', 'Valor':'Valor_Orcamento'})
     orcamento_mensal_debito
     debito_mes_consolidado =  pd.merge(debito_mes,orcamento_mensal_debito, on='Mês Referência',how='outer' )
-    debito_mes_consolidado
+    debito_mes_consolidado['Valor_Orcamento'] = debito_mes_consolidado['Valor_Orcamento'].str.replace(',','.')
     grafico_debito =  go.Figure()
     grafico_debito.add_trace(go.Bar(x=debito_mes_consolidado['Mês Referência'], y=debito_mes_consolidado['Valor'] ))
     grafico_debito.add_trace(go.Bar(x=debito_mes_consolidado['Mês Referência'], y=debito_mes_consolidado['Valor_Orcamento'] ))
