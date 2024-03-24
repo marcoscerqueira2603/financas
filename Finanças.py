1641c18f7ab4294e5dce9e5d7d790d0c78764422
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-03-24 00:10:22 -0300

    aaa

--- Finanças.py
+++ Finanças.py
@@ -325,7 +325,7 @@ with tab1:
 
         orcamento_mensal_salario = orcamento_mensal[orcamento_mensal['Tipo Orçamento'] == 'Salário'] 
         orcamento_mensal_sem_salario = orcamento_mensal[orcamento_mensal['Tipo Orçamento'] != 'Salário']
-        orcamento_mensal_sem_salario = orcamento_mensal[orcamento_mensal['Tipo Orçamento'] != 'Patrimonio que sobrou de 2023']
+        orcamento_mensal_sem_salario = orcamento_mensal_sem_salario[orcamento_mensal_sem_salario['Tipo Orçamento'] != 'Patrimonio que sobrou de 2023']
         orcamento_mensal_sem_salario = orcamento_mensal_sem_salario.drop('Tipo Orçamento', axis=1)
         orcamento_mensal_sem_salario              
 
