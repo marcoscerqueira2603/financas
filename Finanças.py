7a03f6d5d80e81d05a6cc01dd1bcd1a3c1cdfd2d
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-04-14 23:05:08 -0300

    aaaa

--- Finanças.py
+++ Finanças.py
@@ -421,8 +421,14 @@ with tab4:
     credito_mes_nmeses = debito['Mês Referência'].nunique()
     credito_mes_nmeses_bdfiltrada = credito_mes.iloc[0:credito_mes_nmeses, :]
     credito_mes_nmeses_bdfiltrada['Saldo'] = credito_mes_nmeses_bdfiltrada['Valor_Orcamento'] - credito_mes_nmeses_bdfiltrada['Valor']
-    credito_mes_nmeses_bdfiltrado = round(credito_mes_nmeses_bdfiltrada['Saldo'].sum(),2)
-    st.metric(label='Saldo até o mês atual',value=credito_mes_nmeses_bdfiltrado)
+    credito_mes_nmeses_bdfiltrado_saldo = round(credito_mes_nmeses_bdfiltrada['Saldo'].sum(),2)
+    credito_mes_nmeses_bdfiltrado_real = round(credito_mes_nmeses_bdfiltrada['Valor'].sum(),2)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric(label='Saldo até o mês atual',value=credito_mes_nmeses_bdfiltrado_saldo)
+    with col2:
+        st.metric(label='Saldo até o mês atual',value=credito_mes_nmeses_bdfiltrado_real)
     
 
     
