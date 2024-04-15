feb5ad0ba50b5578aad53b3efc49a17b4e0971c1
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-04-14 22:31:19 -0300

    aaa

--- Finanças.py
+++ Finanças.py
@@ -417,7 +417,11 @@ with tab4:
     credito = credito.sort_values(by='Mês')
     credito_mes = credito.groupby(['Mês'])['Valor'].sum()
     credito_mes = pd.merge(credito_mes, orcamento_mensal_credito, on ='Mês', how='outer')
-    credito_mes
+
+    credito_mes_nmeses = debito_mes['Mês Referência'].nunique()
+    credito_mes_nmeses
+
+
     grafico_credito =  go.Figure()
     grafico_credito.add_trace(go.Bar(x= credito_mes['Mês'], y= credito_mes['Valor_Orcamento'],
                                      name='Orçado',
