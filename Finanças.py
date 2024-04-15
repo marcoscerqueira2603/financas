c390ef4c9fde8f2fddc49aa105029a572fd86859
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-04-14 21:44:55 -0300

    aaa

--- Finanças.py
+++ Finanças.py
@@ -416,7 +416,7 @@ with tab4:
 
     credito = credito.sort_values(by='Mês')
     credito_mes = credito.groupby(['Mês'])['Valor'].sum()
+    credito_mes = pd.merge(credito_mes, orcamento_mensal_credito, on ='Mês', how='Outer')
     credito_mes
-
     orcamento_mensal_credito
     
