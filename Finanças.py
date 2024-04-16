e100f66148df8ed954f6c739ea6fda03dec99d05
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-04-15 22:16:25 -0300

    aaaa

--- Finanças.py
+++ Finanças.py
@@ -487,6 +487,7 @@ with tab5:
     vr['Valor'] = vr['Valor'].str.replace(',', '.')
     vr['Valor'] = vr['Valor'].astype(float)
     vr_agrupado = vr.groupby(['Mês Referência'])['Valor'].sum()
-    vr_agrupado
+    vr_agrupado = vr_agrupado.rename(columns={'Mês': 'Mês Referência'}) 
+    vr_agrupado = pd.merge(vr_agrupado, orcamento_mensal, on='Mês Referência ', how='outer')
     orcamento_mensal
     orcamento_mensal_vr
